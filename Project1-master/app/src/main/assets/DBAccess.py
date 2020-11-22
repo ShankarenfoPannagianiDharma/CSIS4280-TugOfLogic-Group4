30de9da14f4a5a69a6e263dca07aa66ddef910fa
ShankarenfoPannagianiDharma <dharmas@douglascollege.ca>
2020-11-21 19:01:47 -0800

    Update DBAccess.py
    DBAccess has wrong code. fixed now.

--- Project1-master/app/src/main/assets/DBAccess.py
+++ Project1-master/app/src/main/assets/DBAccess.py
@@ -1,35 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import json
+import os
 
-# sudo pip3 install Flask-PyMongo
-# sudo pip3 install pymongo[srv]
+app = Flask(__name__)   #app is an instance of FLASK
 
+@app.route('/SessionCreate',methods=['POST'])  # POST only
+def addSessionJSON():
+    # POST data: first_name & last_name
+    data = request.get_json();     #data is from application/json form data
+    if os.path.exists('./SessionLobbies.json'):
+        # append to json file
+        with open("SessionLobbies.json", "r+") as file:  
+            jsonDictionary = json.load(file)
+            jsonDictionary.append(data)
+            file.seek(0)
+            json.dump(jsonDictionary, file)
+    else:   #Create file if not exists
+        jsonList = []
+        jsonList.append(data)
+        with open("SessionLobbies.json","w+") as file:    
+            json.dump(jsonList, file)
+    
+    return '<h1>Operation complete. Data {} added.</h1>'.format(data)
 
-from flask import Flask, render_template,  request, escape
-from flask_pymongo import PyMongo
-
-
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://admin:root@csis4280cluster.7xiy1.mongodb.net/sample_mflix?retryWrites=true&w=majority" # replace the URI with your own connection
-mongo = PyMongo(app)
-
-
-@app.route('/')
-def init():                            # this is a comment. You can create your own function name
-    return '<h1> {} </h1>'.format(__name__)
-
-
-
-
-@app.route('/data')
-def show_data():
-    theMovie = mongo.db.movies.find({}).limit(1)
-    return render_template("data.html",movieInfo=theMovie,collectionNames = mongo.db.collection_names())
-
-
-@app.route('/moredata')                  # http://3.237.92.38:5000/moredata
-def show_alldata():
-    theMovie = list(mongo.db.movies.find({}).limit(20))
-    return render_template("data2.html",movieCollection=theMovie,collectionNames = mongo.db.collection_names())
-
+@app.route('/GetSessions')
+def getSessionJSON():
+    #open JSON file
+    reader = open("SessionLobbies.json", "r")
+    #return the String
+    return reader.read()
+    
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',debug=True)   # host='0.0.0.0' means whatever your public ip assigned will be used
